ce431f430cdd500d5e01fd2d28334824aab480f2
Barri <info@bdesignstudio.co.uk>
2020-12-02 16:19:07 +0000

    issue with input

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -7,3 +7,15 @@ const dom = await render('index.html');//render in async fn
 const input = dom.window.document.querySelector('input');
 assert(input)
 });
+it ('shows a success message with a valid email' , async ()=>{
+  const dom = await render('index.html');
+  const input = dom.window.document.querySelector('input');
+  input.value = 'zart@zartan.com'
+
+ dom.window.document
+ .querySelector('form')
+ .dispatchEvent(new dom.window.Event('submit'));
+
+ const h1 = dom.window.document.querySelector('h1');
+ console.log('Contents of h1',h1.innerHTML);
+})
